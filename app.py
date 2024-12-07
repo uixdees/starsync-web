d60414ededc47d1bb5b3bc72767612148b4048ad
Дмитрий Давыдов <d.davidov@MacBook-Pro.local>
2024-12-07 12:45:11 +0300

    обновление 10

--- app.py
+++ app.py
@@ -1,26 +1,9 @@
 from flask import Flask, render_template
-from flask_talisman import Talisman
-
 app = Flask(__name__)
 
-# Вариант 1: Полная настройка с Talisman
-talisman = Talisman(
-    app,
-    content_security_policy={
-        'default-src': '\'self\'',
-        'script-src': [
-            '\'self\'', 
-            '\'unsafe-inline\'', 
-            'https://cdnjs.cloudflare.com'
-        ],
-        'style-src': [
-            '\'self\'', 
-            '\'unsafe-inline\''
-        ],
-        'img-src': [
-            '\'self\'', 
-            'data:'
-        ]
-    },
-    content_security_policy_nonce_in=['script-src']
-)
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
